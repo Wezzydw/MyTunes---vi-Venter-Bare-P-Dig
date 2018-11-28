67d38a273d005bc387eebc510644ac39aa90717f
Wezzydw <44640169+Wezzydw@users.noreply.github.com>
2018-11-28 11:34:29 +0100

    UI

--- MyTunes/src/mytunes/MyTunes.java
+++ MyTunes/src/mytunes/MyTunes.java
@@ -5,9 +5,12 @@
  */
 package mytunes;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -19,33 +22,17 @@ import javafx.stage.Stage;
  */
 public class MyTunes extends Application {
     
-    int id;
-    String name;
     public MyTunes()
     {
-        this.id = 0;
-        
+      
+   
     }
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/mytunes/gui/view/MyTunesMainView.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     /**
--- MyTunes/src/mytunes/gui/controller/MyTunesMainViewController.java
+++ MyTunes/src/mytunes/gui/controller/MyTunesMainViewController.java
@@ -7,7 +7,17 @@ package mytunes.gui.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 
 /**
  * FXML Controller class
@@ -16,6 +26,23 @@ import javafx.fxml.Initializable;
  */
 public class MyTunesMainViewController implements Initializable {
 
+    @FXML
+    private TextArea txtTopleft;
+    @FXML
+    private Slider sliderVol;
+    @FXML
+    private ListView<?> listViewLibrary;
+    @FXML
+    private ListView<?> listViewSongInfo;
+    @FXML
+    private ListView<?> listViewQueue;
+    @FXML
+    private ComboBox<?> comboBoxMisc;
+    @FXML
+    private TextField txtFieldSearch;
+    @FXML
+    private ListView<?> listViewAllSongs;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +50,66 @@ public class MyTunesMainViewController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void onHandleSliderVol(MouseEvent event) {
+        System.out.println("VOLUME!");
+    }
+
+    @FXML
+    private void onHandleShuffe(ActionEvent event) {
+    }
+
+    @FXML
+    private void onHandleRepeat(ActionEvent event) {
+    }
+
+    @FXML
+    private void onHandlePrev(ActionEvent event) {
+    }
+
+    @FXML
+    private void onHandlePlay(ActionEvent event) {
+    }
+
+    @FXML
+    private void onHandlePause(ActionEvent event) {
+    }
+
+    @FXML
+    private void onHandleNext(ActionEvent event) {
+    }
+
+    @FXML
+    private void onHandleAdd(ActionEvent event) {
+    }
+
+    @FXML
+    private void onHandleRemove(ActionEvent event) {
+    }
+
+    @FXML
+    private void onHandleMisc(ActionEvent event) {
+    }
+
+    @FXML
+    private void onHandleSearch(KeyEvent event) {
+    }
+
+    @FXML
+    private void onHandlePlaylistEdit(ActionEvent event) {
+    }
+
+    @FXML
+    private void onHandlePlaylistAdd(ActionEvent event) {
+    }
+
+    @FXML
+    private void onHandlePlaylistRemove(ActionEvent event) {
+    }
+
+    private void onHandleSliderVol(DragEvent event) {
+        System.out.println("hej");   
+    }
     
 }
