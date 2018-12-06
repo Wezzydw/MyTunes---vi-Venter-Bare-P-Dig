ff7884ea1baa179baf2faf238fc62def6776129a
andr4229 <andr4229@hotmail.com>
2018-12-06 10:37:21 +0100

    settingsDAO

--- MyTunes/src/mytunes/dal/SettingsDAO.java
+++ MyTunes/src/mytunes/dal/SettingsDAO.java
@@ -5,9 +5,15 @@
  */
 package mytunes.dal;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import mytunes.be.Playlist;
 import mytunes.be.Queue;
 import mytunes.be.Song;
+import mytunes.bll.Player;
 
 /**
  *
@@ -15,22 +21,69 @@ import mytunes.be.Song;
  */
 public class SettingsDAO
 {
+    DatabaseConnection conProvider;
+    Player player;
+    public void updateVolume()
+    {
+        try (Connection con = conProvider.getConnection()) {
+            PreparedStatement statement = (PreparedStatement) con.createStatement();
+            statement.executeQuery("SELECT * FROM Songs");
+            statement.executeQuery("UPDATE Settings SET Volume = "
+                    + "player.getvolume eller noget" + ";");
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        
+    }
     public double lastSetVolume()
     {
-        
-        return 2.11;
+        double vol = 0;
+        try (Connection con = conProvider.getConnection()) {
+            //PreparedStatement pstmt = con.prepareStatement("SELECT * FROM Settings;");
+            Statement statement = con.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM Settings;");
+            while (rs.next()) {
+                vol = rs.getDouble("Volume");
+                
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return vol;
     }
-    public Song lastPlayedSong()
-    {
-        return null;
+    public int lastPlayedSong()
+    {   
+        int songId = 0;
+        try (Connection con = conProvider.getConnection()) {
+            //PreparedStatement pstmt = con.prepareStatement("SELECT * FROM Settings;");
+            Statement statement = con.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM Settings;");
+            while (rs.next()) {
+                songId = rs.getInt("lastSong");
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return songId;
     }
     public Playlist lastPlayedPlaylist()
     {
         return null;
     }
-    public Queue lastPlayedQueue()
+    public String lastPlayedQueue()
     {
-        return null;
+        String queueList = "";
+        try (Connection con = conProvider.getConnection()) {
+            //PreparedStatement pstmt = con.prepareStatement("SELECT * FROM Settings;");
+            Statement statement = con.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM Settings;");
+            while (rs.next()) {
+                queueList = rs.getString("lastQueue");
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return queueList;
     }
     
 }
